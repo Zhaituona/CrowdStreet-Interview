9eb88076d965faafa5642e20f85039b2d843c6de
Zhaituona <zaytuna128@gmail.com>
2022-04-23 01:11:51 -0400

    added new cases

--- Cars/src/main/java/runner/TestRunner.java
+++ Cars/src/main/java/runner/TestRunner.java
@@ -5,14 +5,14 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features={"C:\\Users\\Elzat\\IdeaProjects\\CarsBDD\\Cars\\src\\main\\java\\features\\Login.feature"},
+@CucumberOptions(features={"C:\\Users\\Elzat\\IdeaProjects\\CarsBDD\\Cars\\src\\main\\java\\features"},
         glue = {"stepDefinition"},
         format = {"pretty","json:report/cucumber.json","html:report/cucumber-pretty"},
         monochrome = true,
         strict = true,
-        dryRun = false
+        dryRun =false
+
 )
-
-
 public class TestRunner {
+
 }
